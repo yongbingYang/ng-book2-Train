4d33fc15d6810f63bff5c2b446489d087a52da9d
杨勇兵 <yongbing.yang@xc-smart.cn>
2019-04-16 14:35:54 +0800

    提交对 echarts 的导入

--- angular2-echarts/src/app/app.component.ts
+++ angular2-echarts/src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'angular2-echarts';
+
+  public chartOption: any;
+
+  ngOnInit(): void {
+    this.chartOption = {
+      title: {
+        text: 'ECharts 入门示例'
+      },
+      tooltip: {},
+      legend: {
+        data: ['销量']
+      },
+      xAxis: {
+        data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+      },
+      yAxis: {},
+      series: [{
+        name: '销量',
+        type: 'bar',
+        data: [5, 20, 36, 10, 10, 20]
+      }]
+    };
+  }
+
 }
